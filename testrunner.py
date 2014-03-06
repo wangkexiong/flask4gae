38780cb0e13cdc7197b0c1ec58f38b7277e4a883
wangkexiong <wangkexiong@gmail.com>
2014-03-06 18:52:20 +0800

    Move testcase outside of GAE upload source

--- testrunner.py
+++ testrunner.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
 import optparse
 import sys
 import unittest2
@@ -11,6 +10,9 @@ Run unit tests for App Engine apps.
 
 SDK_PATH    Path to the SDK installation"""
 
+SOURCECODE_PATH = 'source'
+TESTSCRIPT_PATH = 'testcase'
+
 
 def main(sdk_path, test_path):
     #Fix sys path for App Engine SDK
@@ -19,9 +21,13 @@ def main(sdk_path, test_path):
     dev_appserver.fix_sys_path()
 
     #Fix sys path for Flask application
-    sys.path.insert(0, "src")
+    sys.path.insert(0, SOURCECODE_PATH)
 
-    import runner
+    import appengine_config     # Import 3rd party libraries
+    import logging
+    logger = logging.getLogger('utlogger')
+    logger.info("Start Running TestCases")
+
     suite = unittest2.loader.TestLoader().discover(test_path)
     unittest2.TextTestRunner(verbosity=2).run(suite)
 
@@ -33,6 +39,7 @@ if __name__ == '__main__':
         print 'Error: Exactly 1 argument required.'
         parser.print_help()
         sys.exit(1)
+
     SDK_PATH = args[0]
-    TEST_PATH = os.path.join("src", "tests")
-    main(SDK_PATH, TEST_PATH)
+
+    main(SDK_PATH, TESTSCRIPT_PATH)
