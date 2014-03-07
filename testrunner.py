e5c9c2bf7b0b2a45d98337194f097727c2411e2b
wangkexiong <wangkexiong@gmail.com>
2014-03-07 14:08:28 +0800

    unittest2 log redirect to logging framework

--- testrunner.py
+++ testrunner.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import optparse
 import sys
+import optparse
+import logging
 import unittest2
 
 
@@ -14,6 +15,33 @@ SOURCECODE_PATH = 'source'
 TESTSCRIPT_PATH = 'testcase'
 
 
+class LoggerStream(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def flush(self):
+        for handler in self.logger.handlers:
+            handler.flush()
+
+    def write(self, buf):
+        sepos = buf.rfind('\n')
+
+        if sepos != -1:
+            content = self.linebuf + buf[:sepos]
+            self.linebuf = buf[sepos:].lstrip()
+
+            for line in content.splitlines():
+                self.logger.log(self.log_level, line)
+        else:
+            self.linebuf += buf
+
+
 def main(sdk_path, test_path):
     #Fix sys path for App Engine SDK
     sys.path.insert(0, sdk_path)
@@ -24,12 +52,14 @@ def main(sdk_path, test_path):
     sys.path.insert(0, SOURCECODE_PATH)
 
     import appengine_config     # Import 3rd party libraries
-    import logging
     logger = logging.getLogger('utlogger')
     logger.info("Start Running TestCases")
+    logger.info("")
+
+    stream = LoggerStream(logger, logging.INFO)
 
     suite = unittest2.loader.TestLoader().discover(test_path)
-    unittest2.TextTestRunner(verbosity=2).run(suite)
+    unittest2.TextTestRunner(stream, verbosity=2).run(suite)
 
 
 if __name__ == '__main__':
